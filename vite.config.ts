8b9d3ff026c949889ad5d7748fc58b7cb3959a09
Shivam Gupta <118615006+Shivamganesh@users.noreply.github.com>
2025-07-01 11:58:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import path from 'path';
 
-
-export default defineConfig(({ mode }) => ({
-  //  base: '/Shivam-Gupta-Developer-Portfolio/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-   optimizeDeps: {
-    include: ['pdfjs-dist/build/pdf.worker.entry']
-  }
- 
-}));
+  build: {
+    outDir: "dist",
+  },
+ server: {
+  // remove: historyApiFallback
+  port: 3000, // ✅ you can keep other config here
+}
+
+});
