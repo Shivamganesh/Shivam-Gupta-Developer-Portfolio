475d0b1d5324eaa866abe28a86869a095f28f9bf
Shivam Gupta <118615006+Shivamganesh@users.noreply.github.com>
2025-07-01 12:29:05 +0530

    Update pdf.worker.js

--- src/utils/pdf.worker.js
+++ src/utils/pdf.worker.js
@@ -1,1 +1,2 @@
-importScripts("https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js");
+// src/utils/pdf.worker.js
+export default "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
